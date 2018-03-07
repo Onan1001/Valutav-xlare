46c51bce907716d6078cf2a39ae8b2e89a8efca6
onan1001 <onan1001@gmail.com>
2018-03-07 14:04:12 +0100

    removed string

--- Valutaväxlare/Program.cs
+++ Valutaväxlare/Program.cs
@@ -10,7 +10,7 @@ namespace Valutaväxlare
     {
         static void Main(string[] args)
         {
-            string hej = "nikke";
+          
             string valuta1 = null;
             string valuta2 = null;
             double tal = 0;
